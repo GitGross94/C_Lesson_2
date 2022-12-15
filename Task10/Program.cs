0c40aa72cd2e3aba8ca803fe99bf3f86d57bf7eb
Boris <twinlab.forever@yandex.ru>
2022-12-16 02:21:30 +0500

    Задача 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,6 @@
-﻿int[] array = {1,5,11};
-Console.WriteLine(array[1]);
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+int num1 = num / 10;
+int num2 = num1 % 10;
+Console.Write(num2);
